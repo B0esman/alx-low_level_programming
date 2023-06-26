931185747c2433024a5bd858017a768888faadf8
B0esman <mphonche1@gmail.com>
2023-06-26 06:21:53 -0700

    3-put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,13 @@
 /**
  * puts - prints a string, followed by a new line to stdout
  * @str: string to print
- * Return: string and new line
+ * Return: void
  */
-
-int void _puts(char *str)
+void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i])
+	for (*str != '\0'; str++)
 	{
-	_putchar(str[i]);
-	i++;
+	_putchar(*str);
 	}
 	_putchar('\n');
 }
