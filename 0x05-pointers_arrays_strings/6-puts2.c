01985b1014274f5e53632313fb802b3085d89382
B0esman <mphonche1@gmail.com>
2023-06-27 16:29:21 -0700

    7-puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other charater of a string
+ * @str: string reference
+ * Return: 0
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+if (str[i] == '\0')
+{
+}
+while (*(str + i) != '\0')
+{
+_putchar(*(str = i));
+i = i = 2;
+}
+_putchar('\n');
+}
